bf18cefec27c11b9164da597fd153f60f95b6fb4
unknown <muktinathrajbanshi786@gmail.com>
2026-04-14 21:42:27 +0545

    aja lai yat yar

--- frontend/src/pages/Porjects.tsx
+++ frontend/src/pages/Porjects.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import type { Project } from "../types"
 
 const Porjects = () => {
+
+  const { projectId } = useParams()
+  const navigate = useNavigate()
+
+  const [project, setProject] = useState<Project | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  const [isGenerating, setIsGenerating] = useState(true)
+  const [device, setDevice] = useState<"phone" | "tablet" | "desktop">("desktop")
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isSaving, setIsSaving] = useState(false)
+
+  const fetchProject = async () => {
+
+  }
+
+  useEffect(() => {
+    fetchProject()
+  }, [])
+
+
   return (
     <div>
       <h1>Projects</h1>
