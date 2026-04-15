6f163d240485bd75f140ac047a5ee89edbe26561
unknown <muktinathrajbanshi786@gmail.com>
2026-04-15 21:29:42 +0545

    hait nidra over lagna thalyo yar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import type { Project } from "../types";
+import type { Message, Project } from "../types";
 
 interface SidebarProps {
     isMenuOpen: boolean;
@@ -13,7 +13,30 @@ const Sidebar = ({isMenuOpen, project, setProject, isGenerating, setIsGenerating
   return (
     <div className={`h-full sm:max-w-sm rounded-xl bg-gray-900 border-gray-800
     transition-all ${isMenuOpen ? "max-sm:w-0 overflow-hidden" : "w-full"}`}>
-      
+      <div className="flex flex-col h-full">
+        {/* Messages container  */}
+        <div className="flex-1 overflow-y-auto no-scrollbar px-3 flex flex-col
+        gap-4">
+            {[...project.conversation, ...project.versions]
+            .sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).
+            getTime()).map((message) =>{
+                const isMessage = "content" in message;
+
+                if(isMessage) {
+                    const msg = message as Message;
+                    const isUser = msg.role === "user";
+                    return (
+                        <div key={msg.id} className={`flex items-center gap-3 $
+                        {isUser ? "justify-end"} `}>
+
+                        </div>
+                    )
+                }
+            })}
+        </div>
+        {/* Input area  */}
+        <form></form>
+      </div>
     </div>
   )
 }
