feb70cea1d90d1b0c5d1938c9af04bcbf560c72e
unknown <muktinathrajbanshi786@gmail.com>
2026-04-17 21:20:59 +0545

    hait nidra over lagyo yar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Preview from "./pages/Preview"
 import Community from "./pages/Community"
 import View from "./pages/View"
 import Navbar from "./components/Navbar"
+import { Toaster } from "sonner"
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
 
   return (
     <div>
+      <Toaster />
       {!hideNavbar && <Navbar />}
       
       <Routes>
---
+++ frontend/src/providers.tsx
@@ -0,0 +1,18 @@
+import { AuthUIProvider } from "@daveyplate/better-auth-ui"
+import { authClient } from "@/lib/auth-client";
+import { NavLink, useNavigate } from "react-router-dom";
+
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const navigate = useNavigate()
+
+    return (
+        <AuthUIProvider
+        authClient={authClient}
+        navigate={navigate}
+        Link={(props) => <NavLink {...props} to={props.href} />}
+        >
+            {children}
+        </AuthUIProvider>
+    )
+}
