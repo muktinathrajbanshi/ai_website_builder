f24addd1a8734ce909e5e683d4ec536097f7a374
unknown <muktinathrajbanshi786@gmail.com>
2026-04-18 22:21:16 +0545

    aja lai yati yar la aba voli

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
 import userRouter from './routes/userRoutes.js';
+import projectRouter from './routes/projectRoutes.js';
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/project", projectRouter)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
