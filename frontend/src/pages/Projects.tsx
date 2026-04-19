47c3546f357397ca33bb968645932c40bfc4c1b5
unknown <muktinathrajbanshi786@gmail.com>
2026-04-19 17:48:02 +0545

    jiu dukho my lord

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -2,9 +2,9 @@ import { useEffect, useRef, useState } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import type { Project } from "../types"
 import { ArrowBigDownDashIcon, EyeIcon, EyeOffIcon, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, SmartphoneIcon, TabletIcon, XIcon } from "lucide-react"
-import { dummyConversations, dummyProjects, dummyVersion } from "../assets/assets"
 import Sidebar from "../components/Sidebar"
 import ProjectPreview, { type ProjectPreviewRef } from "../components/ProjectPreview"
+import api from "@/configs/axios"
 
 const Porjects = () => {
 
@@ -23,14 +23,16 @@ const Porjects = () => {
   const previewRef = useRef<ProjectPreviewRef>(null)
 
   const fetchProject = async () => {
-    const project = dummyProjects.find(project => project.id === projectId)
-    setTimeout(() => {
-      if(project){
-        setProject({...project, conversation: dummyConversations, versions: dummyVersion});
-        setLoading(false)
-        setIsGenerating(project.current_code ? false : true)
-      }
-    }, 2000)
+    try {
+      const { data } = api.get(`/api/user/project/${projectId}`)
+      setProject(data.project)
+      setIsGenerating(data.project.current_code ? false : true)
+      setLoading(false)
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+      
+    }
   }
 
   const saveProject = () => {
@@ -59,8 +61,11 @@ const Porjects = () => {
   }
 
   useEffect(() => {
-    fetchProject() 
-  }, [])
+    if(project && !project.current_code) {
+      const intervalId = setInterval(fetchProject, 10000);
+      return () => clearInterval(intervalId)
+    }
+  }, [project])
 
   if(loading) {
     return (
