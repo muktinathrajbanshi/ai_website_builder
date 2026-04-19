5f14246ee13cda8652107230c248a8f622fdb39b
unknown <muktinathrajbanshi786@gmail.com>
2026-04-19 23:26:01 +0545

    yaha pani

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -53,7 +53,7 @@ export const makeRevision = async (req: Request, res: Response) => {
 
         // Enhance user prompt
         const promptEnhanceResponse = await openai.chat.completions.create({
-            model: "z-ai/glm-4.5-air:free",
+            model: "kwaipilot/kat-coder-pro:free",
             messages: [
                 {
                     role: "system",
@@ -95,7 +95,7 @@ export const makeRevision = async (req: Request, res: Response) => {
 
         // Generate website code
         const codeGenerationResponse = await openai.chat.completions.create({
-            model: "z-ai/glm-4.5-air:free",
+            model: "kwaipilot/kat-coder-pro:free",
             messages: [
                 {
                     role: "system",
